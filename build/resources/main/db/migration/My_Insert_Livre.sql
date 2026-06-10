969083ea829a1aedd67e2a1b01b3b5f0068a2dae
servonnm <mat.servonnet@gmail.com>
2026-06-10 21:41:09 +0200

    Remove dossier inutile

--- build/resources/main/db/migration/My_Insert_Livre.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO biblio.livre (titre, nb_pages, editeur, date_publication, created_at, updated_at, auteur_id) VALUES
-('Les Misérables', 1232, 'A. Lacroix', '1862-01-01', NOW(), NOW(), 1),
-('Fictions', 224, 'Sur', '1944-01-01', NOW(), NOW(), 2),
-('Jacques le fataliste', 384, 'Garnier', '1796-01-01', NOW(), NOW(), 3);
